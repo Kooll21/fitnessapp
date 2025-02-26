4b7aaa798bece404c8733865209782bc7d7a11da
Kooll21 <z.lebedev@seznam.cz>
2025-02-26 09:11:13 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -55,7 +55,6 @@ function displayWorkouts(workouts) {
 function showWorkoutDetails(index) {
     const workoutData = workoutsData[index];
     if (!workoutData) {
-        console.error(`Тренировка с индексом ${index} не найдена в workoutsData`); // Исправлена строка 58
         alert("Ошибка: данные тренировки недоступны");
         return;
     }
